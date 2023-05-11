95fc0c7a479f3c8e201dbeb57e1f259f7f9252d0
survani <giovaniportal@gmail.com>
2023-05-10 20:20:27 -0700

    Added env variables correctly

--- src/app/components/streamercard.tsx
+++ src/app/components/streamercard.tsx
@@ -1,47 +1,78 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 const StreamerCard = () => {
 	type Streams = {
 		id: number;
+		user_id: number;
 		user_name: string;
 		type: string;
 		viewer_count: number;
 		thumbnail_url: string;
 	};
 
+	type User = {
+		user_id: number;
+		profile_image_url: string;
+	};
+
 	//FIXME: Make sure we add enviroment variables here before we push...
-	async function getUsers() {
-		const res = await fetch('https://api.twitch.tv/helix/streams?first=50', {
+	async function getStreams() {
+		const res = await axios('https://api.twitch.tv/helix/streams?first=50', {
 			method: 'get',
 			headers: {
-				Authorization: `Bearer ${'wgscm1el3g8mipk3qckv4vov6tyo5u'}`,
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_TWITCH_TOKEN}`,
 				'Content-Type': 'application/json',
-				'Client-Id': 'g9hjo173mm3ngrjedk6njpgl1m4xo6',
+				'Client-Id': `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
 			},
 		});
-		const streams = await res.json();
+		const streams = await res.data;
 		return streams.data;
 	}
 
-	const { data, isLoading, isFetching, error } = useQuery({
-		queryKey: ['hydrate-streams'],
-		queryFn: () => getUsers(),
+	const {
+		data: streams,
+		isLoading,
+		isFetching,
+		error,
+	} = useQuery({
+		queryKey: ['streams'],
+		queryFn: getStreams,
 	});
 
-	async function getOne() {
-		const res = await fetch(`https://api.twitch.tv/helix/users?${data.id}`, {
-			method: 'get',
-			headers: {
-				Authorization: `Bearer ${'wgscm1el3g8mipk3qckv4vov6tyo5u'}`,
-				'Content-Type': 'application/json',
-				'Client-Id': 'g9hjo173mm3ngrjedk6njpgl1m4xo6',
-			},
-		});
-		const streams = await res.json();
-		return streams.data;
+	const allUserId = streams?.map((streamers: Streams) => {
+		return streamers.user_id;
+	});
+
+	console.log;
+
+	async function getUsers() {
+		const res = await axios(
+			`https://api.twitch.tv/helix/users?id=${allUserId}`,
+			{
+				method: 'get',
+				headers: {
+					Authorization: `Bearer ${process.env.NEXT_PUBLIC_TWITCH_TOKEN}`,
+					'Content-Type': 'application/json',
+					'Client-Id': `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
+				},
+			}
+		);
+		const users = await res.data;
+		return users.data;
 	}
+
+	const {
+		data: users,
+		isLoading: load,
+		isFetching: fetched,
+		error: err,
+	} = useQuery({
+		queryKey: ['users'],
+		queryFn: getUsers,
+	});
 
 	return (
 		<>
@@ -50,16 +81,21 @@ const StreamerCard = () => {
 					<p>Oh no, there was an error</p>
 				) : isLoading || isFetching ? (
 					<p>Loading...</p>
-				) : data ? (
+				) : streams ? (
 					<div className='flex flex-wrap justify-center'>
-						{data.map((stream: Streams) => (
+						{streams.map((stream: Streams) => (
 							<div key={stream.id} className='border-2 text-center'>
-								<Image
-									src={`https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.user_name}-200x113.jpg`}
-									alt={stream.user_name}
-									width={250}
-									height={250}
-								/>
+								{/* {users.map((users: User) => (
+									<div key={stream.id} className='border-2 text-center'>
+										<Image
+											src={users.profile_image_url}
+											alt={stream.user_name}
+											width={250}
+											height={250}
+										/>
+									</div>
+								))} */}
+
 								<h3>{stream.user_name}</h3>
 								<h3>Status: {stream.type}</h3>
 								<h3>Viewers: {stream.viewer_count}</h3>
