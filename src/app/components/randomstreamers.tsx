d593cf3b2527d0624c39398e2f53e1ca998b1fe0
survani <giovaniportal@gmail.com>
2023-05-10 21:42:22 -0700

    Removed Forms not needed

--- src/app/components/randomstreamers.tsx
+++ src/app/components/randomstreamers.tsx
@@ -7,34 +7,6 @@ import StreamerCard from './streamercard';
 const RandomStreamers = () => {
 	return (
 		<>
-			<div className='flex justify-end'>
-				<form action='/send-data-here' method='post'>
-					<label htmlFor='search'>Search</label>
-					<input
-						type='text'
-						id='search'
-						name='search'
-						required
-						minLength={10}
-						maxLength={20}
-						className='text-black'
-					/>
-					<button type='submit'>Submit</button>
-				</form>
-
-				<form action='/send-data-here' method='post' className='ml-6'>
-					<label htmlFor='search'>Platforms</label>
-					<input
-						type='text'
-						id='search'
-						name='search'
-						required
-						minLength={10}
-						maxLength={20}
-						className='text-black'
-					/>
-				</form>
-			</div>
 			<div>
 				<StreamerCard />
 			</div>
