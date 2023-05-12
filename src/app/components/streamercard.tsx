a93ae0221565d6065f4fa1ee5a90f9942a1d479d
survani <giovaniportal@gmail.com>
2023-05-11 17:23:21 -0700

    Added Live Icon with Viewer Count

--- src/app/components/streamercard.tsx
+++ src/app/components/streamercard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FireIcon } from '@heroicons/react/24/solid';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Image from 'next/image';
@@ -44,7 +45,10 @@ const StreamerCard = () => {
 						{streams
 							.sort(() => 0.5 - Math.random())
 							.map((stream: Streams) => (
-								<div key={stream.id} className='border-2 text-center m-2 '>
+								<div
+									key={stream.id}
+									className='relative border-2 text-center m-2 '
+								>
 									<Link href={`https://www.twitch.tv/${stream.user_name}`}>
 										<Image
 											src={`https://robohash.org/${randomNumber(
@@ -59,15 +63,22 @@ const StreamerCard = () => {
 											placeholder='blur'
 											blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='
 										/>
+										<div className='absolute top-3 left-2'>
+											{stream.type == 'live' ? (
+												<div className='flex'>
+													<FireIcon className='h-6 w-6 mr-2 text-red-500' />
+													<h3>Viewers: {stream.viewer_count}</h3>
+												</div>
+											) : (
+												<div className='flex'>
+													<FireIcon className='h-6 w-6 mr-2 text-grey-500' />
+													<h3>Offline</h3>
+												</div>
+											)}
+										</div>
 									</Link>
 									<div className='bg-indigo-400 '>
 										<h3 className='text-xl font-bold'>{stream.user_name}</h3>
-										{stream.type == 'live' ? (
-											<h3>Status: {stream.type}</h3>
-										) : (
-											'offline'
-										)}
-										<h3>Viewers: {stream.viewer_count}</h3>
 									</div>
 								</div>
 							))}
